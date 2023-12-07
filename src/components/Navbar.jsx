fc2b8e4efc76b333f104ae186e6288872a644fac
Arindam Maiti <arindammaiti2018@gmail.com>
2023-12-07 10:04:13 +0530

    update in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,6 +6,7 @@ import { logo, menu, close } from "../assets";
 import { useState } from "react";
 
 const Navbar = () => {
+  // eslint-disable-next-line no-unused-vars
   const [active, setActive] = useState("");
 
   return (
@@ -24,6 +25,15 @@ const Navbar = () => {
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer"> Arindam <span className="sm:block hidden"> Maiti </span></p>
         </Link>
+        
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((link)=>(
+            <li key={link.id} className={`${active === link.title?'text-white':'text-secondary'} hover:text-white text-[18px] font-medium cursor-pointer `} onClick={()=> setActive(link.title)}>
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+
       </div>
     </nav>
   );
